aa4eb48962049528912452e3a7452c166a9815b1
Grant Iverson <iversonga@gmail.com>
2018-06-14 20:41:43 -0400

    Converted search-text-container into a form so 'enter' initiates a search

--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -2,21 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SearchFilter = (props) => {
-  this.propTypes = {
-    handleSearchText: PropTypes.func.isRequired,
-    handlePriceFilter: PropTypes.func.isRequired,
-    handleRatingFilter: PropTypes.func.isRequired,
-    searchForLocations: PropTypes.func.isRequired
-  }
 
   return(
     <div id="search-filter-container">
       <p>Use our Veggie Finder to find restaurants that serve the best vegetarian food in the area!</p>
       <div id="sidebar-inputs-container" role="Search">
-        <div id="search-text-container">
+        <form id="search-text-container" onSubmit={(e) => props.searchForLocations(e)}>
           <input id="search-text" type="text" placeholder="Enter a location" aria-label="Search textbox" onChange={(e) => props.handleSearchText(e.target.value)}></input>
-          <input id="search-text-button" type="button" value="Search" aria-label="Click to search" onClick={props.searchForLocations}></input>
-        </div>
+          <input id="search-text-button" type="button" value="Search" aria-label="Click to search"></input>
+        </form>
         <div className="filter-container">
           <select id="price-filter" aria-label="Filter locations by price" onChange={(e) => props.handlePriceFilter(e.target.value)}>
             <option value="any price">Any price</option>
@@ -38,3 +32,10 @@ const SearchFilter = (props) => {
 }
 
 export default SearchFilter;
+
+SearchFilter.propTypes = {
+  handleSearchText: PropTypes.func.isRequired,
+  handlePriceFilter: PropTypes.func.isRequired,
+  handleRatingFilter: PropTypes.func.isRequired,
+  searchForLocations: PropTypes.func.isRequired
+}
