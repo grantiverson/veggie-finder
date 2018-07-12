2fe6034e35ff5c65ea8965b8beacf89b217436c5
Grant Iverson <iversonga@gmail.com>
2018-07-12 00:11:27 -0400

    Removed hard-coded info about restaurants

--- src/components/App.js
+++ src/components/App.js
@@ -20,134 +20,9 @@ class App extends Component {
     // Information about 13 locations that will display immediately upon load
     locations: [
       {
-        name: 'Dandelion Communitea Cafe;',
-        address: '618 N Thornton Ave, Orlando, FL 32803',
-        rating: 4.5,
-        review_count: 674,
-        price: '$$',
-        coordinates: {latitude: 28.552027, longitude: -81.365711},
-        image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/u_gBxBIpx5r48teO7XykSw/180s.jpg',
-        url: 'https://www.yelp.com/biz/dandelion-communitea-caf%C3%A9-orlando-2?osq=dandelion'
-      },
-      {
-        name: 'Dajen Eats',
-        address: '4845 N Orange Blossom Trl, Orlando, FL 32810',
-        rating: 5,
-        review_count: 30,
-        price: '$$',
-        coordinates: {latitude: 28.602381, longitude: -81.418670},
-        image_url: 'https://s3-media3.fl.yelpcdn.com/bphoto/A7YnuHm-rLc6vOWh_zovXg/180s.jpg',
-        url: 'https://www.yelp.com/biz/dajen-eats-orlando-2?osq=Dajen+Eats'
-      },
-      {
-        name: 'Dixie Dharma',
-        address: '2603 E S St, Orlando, FL 32803',
-        rating: 4.5,
-        review_count: 147,
-        price: '$$',
-        coordinates: {latitude: 28.538881, longitude: -81.349373},
-        image_url: 'https://s3-media3.fl.yelpcdn.com/bphoto/KDb8UrN6mhXwoyHMawifog/180s.jpg',
-        url: 'https://www.yelp.com/biz/dixie-dharma-orlando-2?osq=Dixie+Dharma'
-      },
-      {
-        name: 'Loving Hut',
-        address: '2101 E Colonial Dr, Orlando, FL 32803',
-        rating: 4,
-        review_count: 174,
+        name: 'Please search for a location',
+        rating: 0,
         price: '$',
-        coordinates: {latitude: 28.553386, longitude: -81.354787},
-        image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/G7MmlmbNrEP3DS11kRlulw/180s.jpg',
-        url: 'https://www.yelp.com/biz/loving-hut-orlando?osq=loving+hut'
-      },
-      {
-        name: 'Raw Juicing and Detox',
-        address: '898 E Washington St, Orlando, FL 32801',
-        rating: 4.5,
-        review_count: 58,
-        price: '$$',
-        coordinates: {latitude: 28.543536, longitude: -81.366743},
-        image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/2cd8FNlARME_FP2rQ-YNmw/180s.jpg',
-        url: 'https://www.yelp.com/biz/raw-juicing-and-detox-orlando?osq=Raw+Juicing+and+Detox'
-      },
-      {
-        name: 'Veggie Garden',
-        address: '1216 E Colonial Dr, Orlando, FL 32803',
-        rating: 4.5,
-        review_count: 83,
-        price: '$',
-        coordinates: {latitude: 28.552801, longitude: -81.363701},
-        image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/sUdr1FrrtnM0f6zH9MQQQQ/180s.jpg',
-        url: 'https://www.yelp.com/biz/veggie-garden-orlando-2?osq=Veggie+Garden'
-      },
-      {
-        name: 'Infusion Tea',
-        address: '1600 Edgewater Dr, Orlando, FL 32804',
-        rating: 4,
-        review_count: 436,
-        price: '$',
-        coordinates: {latitude: 28.565833, longitude: -81.389666},
-        image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/YcwSq2oXVfKjtFF5rbyEVw/180s.jpg',
-        url: 'https://www.yelp.com/biz/infusion-tea-orlando'
-      },
-      {
-        name: 'Juice\'d Orlando',
-        address: '12 N Summerlin Ave, Orlando, FL 32801',
-        rating: 4,
-        review_count: 65,
-        price: '$$',
-        coordinates: {latitude: 28.542880, longitude: -81.368618},
-        image_url: 'https://s3-media4.fl.yelpcdn.com/bphoto/VjRWo6jr-KuOr_FV0HBIlA/180s.jpg',
-        url: 'https://www.yelp.com/biz/juiced-orlando-3?osq=juice%27d'
-      },
-      {
-        name: 'Khasiyat',
-        address: '852 W Lancaster Rd, Orlando, FL 32809',
-        rating: 4,
-        review_count: 77,
-        price: '$$',
-        coordinates: {latitude: 28.464931, longitude: -81.389559},
-        image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/53GlgJH46ImFluaf3sjsJQ/180s.jpg',
-        url: 'https://www.yelp.com/biz/khasiyat-indian-restaurant-orlando?osq=khasiyat'
-      },
-      {
-        name: 'Maoz',
-        address: '4200 Conroy Rd, Orlando, FL 32839',
-        rating: 4.5,
-        review_count: 84,
-        price: '$',
-        coordinates: {latitude: 28.485318, longitude: -81.432146},
-        image_url: 'https://s3-media3.fl.yelpcdn.com/bphoto/bR_IpWyyb6Bpe802OT4Kog/180s.jpg',
-        url: 'https://www.yelp.com/biz/maoz-orlando?osq=Maoz'
-      },
-      {
-        name: 'Skyebird',
-        address: '3201 Corrine Dr, Orlando, FL 32803',
-        rating: 4.5,
-        review_count: 104,
-        price: '$',
-        coordinates: {latitude: 28.568161, longitude: -81.343824},
-        image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/-WQi8wu_82flrgC75eX6GQ/180s.jpg',
-        url: 'https://www.yelp.com/biz/skyebird-orlando-2?osq=Skyebird'
-      },
-      {
-        name: 'Nature\'s Table Cafe',
-        address: '8001 S Orange Blossom Trl, Orlando, FL 32809',
-        rating: 3,
-        review_count: 10,
-        price: '$',
-        coordinates: {latitude: 28.446041, longitude: -81.395459},
-        image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/tLf3ug-1YZ4lFc4gOAtwrA/180s.jpg',
-        url: 'https://www.yelp.com/biz/natures-table-orlando-2?osq=nature%27s+table'
-      },
-      {
-        name: 'Sweet Tomatoes',
-        address: '4678 E Colonial Dr, Orlando, FL 32803',
-        rating: 3.5,
-        review_count: 127,
-        price: '$$',
-        coordinates: {latitude: 28.552238, longitude: -81.327420},
-        image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/oiMk6yN57BJegG2RKYQLPg/180s.jpg',
-        url: 'https://www.yelp.com/biz/sweet-tomatoes-orlando?osq=sweet+tomatoes+4678+e+colonial+dr'
       }
     ],
     // Google Maps styles
@@ -179,12 +54,14 @@ class App extends Component {
     // State of the rating-filter select tag
     ratingFilterValue: '3',
     // State of the search-text input tag
-    searchValue: '',
+    searchValue: 'Orlando',
     // State of the locations-list-container div
     showList: true
   }
 
   componentDidMount() {
+    this.searchForLocations();
+
     // Creates the Google Maps map script tag, calls the API request, then
     // appends the tag to the end of the document
     function loadError(oError) {
@@ -230,7 +107,7 @@ class App extends Component {
         map: map,
         name: location.name,
         animation: window.google.maps.Animation.DROP,
-        address: location.address,
+        // address: location.location.display_address.join(', '),
         rating: location.rating,
         price: location.price,
         id: i,
@@ -319,7 +196,7 @@ class App extends Component {
 
   // Calls Yelp API and handles return
   searchForLocations = (e) => {
-    e.preventDefault();
+    e ? e.preventDefault() : null;
     // Authorization info for Yelp
     // https://forum.freecodecamp.org/t/authorization-http-header-for-yelp-fusion-api-access-token/140974
     const access_token = "Opsi88BMRhY9PANt58XH8NSBCbDCLLnHL5VLKDmhaOt4qoruzhDzZcqAdCIAAO59a5UvhRFFAqdR6SSZ65VWNpiSsyyX-sLl3TLQNUg1sqi1R-sl4JJQ5QbzqWsUW3Yx";
@@ -329,8 +206,7 @@ class App extends Component {
 
     // Allows access when CORS is disabled
     const cors = "https://cors-anywhere.herokuapp.com/"
-
-    const urlToFetch = `${cors}https://api.yelp.com/v3/businesses/search?term=vegetarian restaurants&location=${this.state.searchValue}&limit=25`
+    const urlToFetch = `${cors}https://api.yelp.com/v3/businesses/search?term=vegetarian restaurants&location=${this.state.searchValue}`
 
     fetch(urlToFetch, {
         headers: myHeaders
@@ -338,26 +214,10 @@ class App extends Component {
         return result.json();
       }).then(places => {
         if (places.businesses[0]) {
-          let updatedBusinesses = [];
-          for (let i = 0; i < places.businesses.length; i++) {
-            const business = places.businesses[i];
-            const updatedBusiness = {
-              name: business.name,
-              address: business.location.display_address.join(', '),
-              rating: business.rating,
-              review_count: business.review_count,
-              price: business.price,
-              coordinates: business.coordinates,
-              image_url: business.image_url,
-              url: business.url,
-            }
-            updatedBusinesses.push(updatedBusiness);
-          }
-
           this.setState({
-            locations: updatedBusinesses
+            locations: places.businesses
           },
-            this.initMap
+            map ? this.initMap : null
           )
         }
 
